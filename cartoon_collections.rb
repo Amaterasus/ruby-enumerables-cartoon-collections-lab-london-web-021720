6b159a1ac17a987210af0ca8bdb56b4a9927f2f3
Skye Geraghty <skyetay03@googlemail.com>
2020-02-10 08:31:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,5 +19,8 @@ def find_the_cheese(maybe_cheese)# code an argument here
   cheese_types = ["cheddar", "gouda", "camembert"]
   
   cheese_index = cheese_types.index { |cheese| maybe_cheese.include?(cheese) }
-
+  
+  if cheese_index
+    cheese_types[cheese_index]
+  end
 end
