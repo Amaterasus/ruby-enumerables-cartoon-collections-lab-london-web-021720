cef08a860985c2d810e88576f0dae4c5efdf897e
Skye Geraghty <skyetay03@googlemail.com>
2020-02-10 08:12:07 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,7 @@ end
 
 def long_planeteer_calls(planeteer_calls)# code an argument here
   # Your code here
-  planeteer_calls.map { |call| call.length > 4}
+  planeteer_calls.any? { |call| call.length > 4}
 
 end
 
